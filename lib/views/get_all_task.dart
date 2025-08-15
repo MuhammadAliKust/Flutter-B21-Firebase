45e38cfc8c305ea95aaec165bc5812af83c6367f
Muhammad Ali <kustalics@gmail.com>
2025-08-15 16:48:21 +0500

    Task Error Handling Done

--- lib/views/get_all_task.dart
+++ lib/views/get_all_task.dart
@@ -64,70 +64,82 @@ class GetAllTaskView extends StatelessWidget {
         initialData: [TaskModel()],
         builder: (context, child) {
           List<TaskModel> taskList = context.watch<List<TaskModel>>();
-          return ListView.builder(
-            itemCount: taskList.length,
-            itemBuilder: (context, i) {
-              return ListTile(
-                leading: Icon(Icons.task),
-                title: Text(taskList[i].title.toString()),
-                subtitle: Text(taskList[i].description.toString()),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Checkbox(
-                      value: taskList[i].isCompleted,
-                      onChanged: (val) async {
-                        try {
-                          await TaskServices().markTaskAsComplete(
-                            taskID: taskList[i].docId.toString(),
-                            isCompleted: val!,
+          return taskList.isNotEmpty
+              ? taskList[0].docId == null
+                    ? Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: taskList.length,
+                        itemBuilder: (context, i) {
+                          return ListTile(
+                            leading: Icon(Icons.task),
+                            title: Text(taskList[i].title.toString()),
+                            subtitle: Text(taskList[i].description.toString()),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Checkbox(
+                                  value: taskList[i].isCompleted,
+                                  onChanged: (val) async {
+                                    try {
+                                      await TaskServices().markTaskAsComplete(
+                                        taskID: taskList[i].docId.toString(),
+                                        isCompleted: val!,
+                                      );
+                                    } catch (e) {
+                                      ScaffoldMessenger.of(
+                                        context,
+                                      ).showSnackBar(
+                                        SnackBar(content: Text(e.toString())),
+                                      );
+                                    }
+                                  },
+                                ),
+                                IconButton(
+                                  onPressed: () async {
+                                    try {
+                                      await TaskServices()
+                                          .deleteTask(
+                                            taskList[i].docId.toString(),
+                                          )
+                                          .then((val) {
+                                            ScaffoldMessenger.of(
+                                              context,
+                                            ).showSnackBar(
+                                              SnackBar(
+                                                content: Text(
+                                                  "Task has been deleted successfully",
+                                                ),
+                                              ),
+                                            );
+                                          });
+                                    } catch (e) {
+                                      ScaffoldMessenger.of(
+                                        context,
+                                      ).showSnackBar(
+                                        SnackBar(content: Text(e.toString())),
+                                      );
+                                    }
+                                  },
+                                  icon: Icon(Icons.delete, color: Colors.red),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            UpdateTaskView(model: taskList[i]),
+                                      ),
+                                    );
+                                  },
+                                  icon: Icon(Icons.edit, color: Colors.blue),
+                                ),
+                              ],
+                            ),
                           );
-                        } catch (e) {
-                          ScaffoldMessenger.of(
-                            context,
-                          ).showSnackBar(SnackBar(content: Text(e.toString())));
-                        }
-                      },
-                    ),
-                    IconButton(
-                      onPressed: () async {
-                        try {
-                          await TaskServices()
-                              .deleteTask(taskList[i].docId.toString())
-                              .then((val) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      "Task has been deleted successfully",
-                                    ),
-                                  ),
-                                );
-                              });
-                        } catch (e) {
-                          ScaffoldMessenger.of(
-                            context,
-                          ).showSnackBar(SnackBar(content: Text(e.toString())));
-                        }
-                      },
-                      icon: Icon(Icons.delete, color: Colors.red),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                UpdateTaskView(model: taskList[i]),
-                          ),
-                        );
-                      },
-                      icon: Icon(Icons.edit, color: Colors.blue),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
+                        },
+                      )
+              : Center(child: Text("No Data Found!"));
         },
       ),
     );
